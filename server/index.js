b4c5040313a274f89d1dd3f482317beb9105123a
Cassie Tarakajian <ctarakajian@gmail.com>
2017-02-13 22:16:53 -0500

    add admin panel for twitter page

--- server/index.js
+++ server/index.js
@@ -21,6 +21,35 @@ app.use(express.static('client'));
 //   });
 // });
 
+require('dotenv').config()
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var word = require('./database.json').word;
+var stream;
+var calm;
+
+function createNewStream(word) {
+  stream = client.stream('statuses/filter', {track: word});
+  stream.on('data', function(event) {
+    io.emit('tweet', event && event.text);
+  });
+   
+  stream.on('error', function(error) {
+    if (error.message == 'Status Code: 420') {
+      calm++;
+    }
+  });
+}
+
+createNewStream(word);
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
@@ -41,6 +70,25 @@ app.get('/interface', function(req, res) {
     res.sendFile(path.join(__dirname + '/../client/interface.html'));
 });
 
+app.get('/admin', function(req, res) {
+  res.sendFile(path.join(__dirname + '/../client/admin.html'));
+});
+
+app.post('/search', function(req, res) {
+  var db = {
+    "word": req.body.searchTerm
+  };
+  io.emit('clear');
+  stream.on('end', function (response) {
+    setTimeout(function () {
+      createNewStream(req.body.searchTerm);
+    }, 1000 * calm);
+  });
+  stream.destroy();
+  fs.writeFile('./server/database.json', JSON.stringify(db, null, 2));
+  res.json({success: true});
+});
+
 
 app.post('/sounds', function(req, res) {  
   var buf = Buffer.from(req.body.file, 'base64');
@@ -54,26 +102,5 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 server.listen(3001);
-
-
-
-require('dotenv').config()
-var Twitter = require('twitter');
-
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
-
-var stream = client.stream('statuses/filter', {track: 'fear'});
-stream.on('data', function(event) {
-  io.emit('tweet', event && event.text);
-});
- 
-stream.on('error', function(error) {
-  throw error;
-});
 
 
